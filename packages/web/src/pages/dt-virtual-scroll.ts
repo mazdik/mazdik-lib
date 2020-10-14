8f907b04f4bee13ea8f61eb1eca2c45077e0f074
mazdik <remappp@gmail.com>
2020-10-14 23:12:10 +0500

    up

--- packages/web/src/pages/dt-virtual-scroll.ts
+++ packages/web/src/pages/dt-virtual-scroll.ts
@@ -2,15 +2,25 @@ import { Page } from '../page';
 import '@mazdik-lib/data-table';
 import { DataTableComponent, Settings, DataTable } from '@mazdik-lib/data-table';
 import { getColumnsPlayers } from '../shared/columns';
+import '@mazdik-lib/crud-table';
+import { CrudTableComponent, CdtSettings, DataManager } from '@mazdik-lib/crud-table';
+import { DemoService } from '../shared/demo.service';
 
 export default class DtVirtualScrollDemo implements Page {
 
   get template(): string {
     return `<p>Client-side virtual scroll with dynamic row height</p>
-    <web-data-table></web-data-table>`;
+    <web-data-table></web-data-table>
+    <p>Server-side virtual scroll</p>
+    <web-crud-table></web-crud-table>`;
   }
 
   load() {
+    this.clientSide();
+    this.serverSide();
+  }
+
+  private clientSide() {
     const component = document.querySelector('web-data-table') as DataTableComponent;
 
     const columns = getColumnsPlayers();
@@ -27,6 +37,17 @@ export default class DtVirtualScrollDemo implements Page {
         table.rows = data;
         table.events.emitLoading(false);
       });
+  }
+
+  private serverSide() {
+    const component = document.querySelector('web-crud-table') as CrudTableComponent;
+    const columns = getColumnsPlayers();
+    const settings = new CdtSettings({
+      virtualScroll: true,
+    });
+    const service = new DemoService();
+    const dataManager = new DataManager(columns, settings, service);
+    component.dataManager = dataManager;
   }
 
 }
