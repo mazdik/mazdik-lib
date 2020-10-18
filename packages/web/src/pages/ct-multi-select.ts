86ba47da978ed8ce711fb2db4adf04a9aa9c7da1
mazdik <remappp@gmail.com>
2020-10-18 20:54:12 +0500

    up

--- packages/web/src/pages/ct-multi-select.ts
+++ packages/web/src/pages/ct-multi-select.ts
@@ -1,28 +1,35 @@
 import { Page } from '../page';
 import '@mazdik-lib/crud-table';
 import { CrudTableComponent, CdtSettings, DataManager } from '@mazdik-lib/crud-table';
-import { ColumnBase, TemplateContext } from '@mazdik-lib/data-table';
+import { ColumnBase } from '@mazdik-lib/data-table';
 import { SelectItem } from '@mazdik-lib/common';
+import '@mazdik-lib/select-list';
+import { SelectListComponent } from '@mazdik-lib/select-list';
 import { DemoService } from '../shared/demo.service';
 import { CellMultiSelectRenderer } from '../shared/cell-multi-select-renderer';
 
 export default class CtMultiSelect implements Page {
 
   get template(): string {
-    return `<div class="multi-select-demo">
-      <web-crud-table></web-crud-table>
+    return `<div class="multi-select-demo-block">
+      <web-select-list class="dt-dropdown-select-list"></web-select-list>
+      <web-crud-table class="multi-select-demo"></web-crud-table>
     </div>`;
   }
 
   load() {
-    const component = document.querySelector('web-crud-table') as CrudTableComponent;
-
     const options: SelectItem[] = [
       {id: '1', name: 'Select 1'},
       {id: '2', name: 'Select 2'},
       {id: '3', name: 'Select 3'},
       {id: '4', name: 'Select 4'},
     ];
+
+    const selectList = document.querySelector('web-select-list') as SelectListComponent;
+    selectList.settings = { multiple: true };
+    selectList.options = options;
+
+    const component = document.querySelector('web-crud-table') as CrudTableComponent;
 
     const columns: ColumnBase[] = [
       { title: 'Id', name: 'id' },
@@ -31,7 +38,7 @@ export default class CtMultiSelect implements Page {
         title: 'Test',
         name: 'test',
         width: 250,
-        cellTemplate: new CellMultiSelectRenderer(this.onClickFunc.bind(this), options),
+        cellTemplate: new CellMultiSelectRenderer(selectList, options),
       }
     ];
     const settings = new CdtSettings({
@@ -40,15 +47,6 @@ export default class CtMultiSelect implements Page {
     const service = new DemoService();
     const dataManager = new DataManager(columns, settings, service);
     component.dataManager = dataManager;
-  }
-
-  private onClickFunc(context: TemplateContext, event: MouseEvent) {
-    const { cell } = context;
-    const row = cell.row;
-    console.log(cell);
-    // const { left, top } = EventHelper.getRowPosition(event, this.dataManager.settings.virtualScroll);
-    // this.rowMenuBeforeOpen(row);
-    // this.rowMenu.show({ originalEvent: event, data: row, left, top } as MenuEventArgs);
   }
 
 }
--- packages/web/src/shared/cell-multi-select-renderer.ts
+++ packages/web/src/shared/cell-multi-select-renderer.ts
@@ -1,17 +1,37 @@
-import { Listener, toggleClass, SelectItem, isBlank } from '@mazdik-lib/common';
-import { TemplateRenderer, TemplateContext, Cell, CellEventType, CellEventArgs } from '@mazdik-lib/data-table';
+import { Listener, toggleClass, SelectItem } from '@mazdik-lib/common';
+import { TemplateRenderer, TemplateContext, Cell, CellEventType, CellEventArgs, EventHelper } from '@mazdik-lib/data-table';
+import { SelectListComponent } from '@mazdik-lib/select-list';
 
 export class CellMultiSelectRenderer implements TemplateRenderer {
 
   private elements = new Map<Cell, HTMLElement>();
+  private inputs = new Map<Cell, HTMLInputElement>();
+  private icons = new Map<Cell, HTMLElement>();
   private listeners: Listener[] = [];
   private editing = {};
-  private isOpen = false;
+  private selectedCell: Cell;
 
-  constructor(
-    private onClickFunc: (event: Event, context: TemplateContext) => void,
-    private options: SelectItem[]
-  ) { }
+  private get isOpen(): boolean { return this._isOpen; }
+  private set isOpen(val: boolean) {
+    this._isOpen = val;
+    this.selectList.isOpen = this.isOpen;
+    this.updateStyleSelectList();
+  }
+  private _isOpen = false;
+
+  constructor(private selectList: SelectListComponent, private options: SelectItem[]) {
+    this.updateStyleSelectList();
+    this.addListener({
+      eventName: 'selectionChange',
+      target: this.selectList,
+      handler: this.onSelectionChange.bind(this)
+    });
+    this.addListener({
+      eventName: 'selectionCancel',
+      target: this.selectList,
+      handler: this.onSelectionCancel.bind(this)
+    });
+  }
 
   create(context: TemplateContext): HTMLElement {
     const { cell, table } = context;
@@ -53,7 +73,10 @@ export class CellMultiSelectRenderer implements TemplateRenderer {
     });
 
     this.elements.set(cell, element);
+    this.inputs.set(cell, input);
+    this.icons.set(cell, icon);
     this.refresh(context);
+
     return element;
   }
 
@@ -64,7 +87,7 @@ export class CellMultiSelectRenderer implements TemplateRenderer {
   }
 
   refresh(context: TemplateContext) {
-    const { cell, table } = context;
+    const { cell } = context;
     const element = this.elements.get(cell);
     if (element) {
       const view = element.firstElementChild as HTMLElement;
@@ -72,12 +95,9 @@ export class CellMultiSelectRenderer implements TemplateRenderer {
       toggleClass(view, 'display-none', this.editing[cell.rowIndex]);
       toggleClass(inputGroup, 'display-none', !this.editing[cell.rowIndex]);
 
-      const selectedName = this.getName(table.rows[cell.rowIndex][cell.column.name]);
-      const input = element.querySelector('input');
-      if (!isBlank(selectedName)) {
-        input.value = selectedName;
-      }
+      this.updateSelectedName(cell);
     }
+    this.updateStyleSelectList();
   }
 
   private addListener(listener: Listener) {
@@ -97,21 +117,33 @@ export class CellMultiSelectRenderer implements TemplateRenderer {
     if (data.type === CellEventType.Click && data.columnIndex === 3) {
       this.editing[data.rowIndex] = true;
     }
+    this.isOpen = false;
   }
 
   private onClick(context: TemplateContext, event: MouseEvent) {
-    const { cell } = context;
-    const element = this.elements.get(cell);
-    if (element) {
-      const icon = element.querySelector('button > i') as HTMLElement;
+    const { cell, table } = context;
+    const icon = this.icons.get(cell);
+    event.stopPropagation();
+    this.isOpen = !this.isOpen;
+    if (icon) {
       toggleClass(icon, 'asc', this.isOpen);
       toggleClass(icon, 'desc', !this.isOpen);
     }
-    event.stopPropagation();
-    this.isOpen = !this.isOpen;
+    const pos = EventHelper.getRowPosition(event, table.settings.virtualScroll);
+    this.selectList.model = cell.value || [];
+    this.selectList.style.left = pos.left + 'px';
+    this.selectList.style.top = pos.top + 'px';
+
+    this.selectedCell = cell;
   }
 
-  private getName(items: any) {
+  private updateSelectedName(cell: Cell) {
+    const selectedName = this.getName(cell.value);
+    const input = this.inputs.get(cell);
+    input.value = selectedName;
+  }
+
+  private getName(items: any): string {
     if (items && items.length && this.options && this.options.length) {
       if (items.length > 1) {
         return items.length + ' items selected';
@@ -120,6 +152,21 @@ export class CellMultiSelectRenderer implements TemplateRenderer {
         return (option) ? option.name : '';
       }
     }
+    return '';
+  }
+
+  private updateStyleSelectList() {
+    this.selectList.style.display = this.isOpen ? 'block' : 'none';
+  }
+
+  private onSelectionChange(event: CustomEvent) {
+    this.selectedCell.value = event.detail;
+    this.updateSelectedName(this.selectedCell);
+    this.isOpen = false;
+  }
+
+  private onSelectionCancel() {
+    this.isOpen = false;
   }
 
 }
